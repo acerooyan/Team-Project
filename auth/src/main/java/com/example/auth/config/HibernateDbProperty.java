997c3c6c1c4272175bcea53472b251ca661f6721
GH-H <huangganghuan1@gmail.com>
2022-01-16 14:22:01 -0800

    gitignore fix

--- auth/src/main/java/com/example/auth/config/HibernateDbProperty.java
+++ auth/src/main/java/com/example/auth/config/HibernateDbProperty.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:database.properties")
 @Getter
 @Setter
 public class HibernateDbProperty {
