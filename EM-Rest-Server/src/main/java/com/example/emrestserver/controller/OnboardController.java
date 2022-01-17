96a670750b832025115efc477c95a9e0a37c11dc
GH-H <huangganghuan1@gmail.com>
2022-01-16 23:07:59 -0800

    postMapping update

--- EM-Rest-Server/src/main/java/com/example/emrestserver/controller/OnboardController.java
+++ EM-Rest-Server/src/main/java/com/example/emrestserver/controller/OnboardController.java
@@ -4,18 +4,29 @@ import com.example.emrestserver.entity.Person;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/em")
+//@RequestMapping("/em")
 public class OnboardController {
 
+    /*
 
+     */
     @PostMapping("/af")
-    public String test2(@RequestBody Person person) {
-        //update database
-
-        return "redirect:http://localhost:4200";
+    public ResponseEntity<Object> person(@RequestBody Person person) {
+        if (person == null
+                || person.getFirstname() == null
+                || person.getLastname()== null) {
+            return ResponseEntity.unprocessableEntity().build();
+        } else {
+            try{
+                //add new person in database
+            }catch (Exception e){
+                return ResponseEntity.internalServerError().build();
+            }
+            System.out.println(person);
+            return ResponseEntity.ok().build();
+        }
     }
 }
