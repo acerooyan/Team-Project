9695fa4f073d7a70eac05fafa05eec5289089478
jcmei89 <jcmei89@gmail.com>
2022-01-16 21:08:06 -0800

    visaStatus

--- EM-Rest-Server/src/main/java/com/example/emrestserver/entity/Address.java
+++ EM-Rest-Server/src/main/java/com/example/emrestserver/entity/Address.java
@@ -5,7 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "contact")
+@Table(name = "address")
 @Data
 @ToString
 @Builder
---
+++ EM-Rest-Server/src/main/java/com/example/emrestserver/entity/VisaStatus.java
@@ -0,0 +1,33 @@
+package com.example.emrestserver.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "visastatus")
+@Data
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VisaStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="ID")
+    private Integer id;
+
+    @Column(name = "VisaType")
+    private String visaType;
+
+    @Column(name = "Active")
+    private Byte active;
+
+    @Column(name = "ModificationDate")
+    private Date modificationDate;
+
+    @Column(name = "CreateUser")
+    private Integer createUser;
+    
+}
