829480e0540369e0dc46e6e64ef9253b4cec727c
jcmei89 <jcmei89@gmail.com>
2022-01-17 19:56:44 -0800

    merge

---
+++ EM-Rest-Server/src/main/java/com/example/emrestserver/domain/MergeDomain.java
@@ -0,0 +1,11 @@
+package com.example.emrestserver.domain;
+
+public class MergeDomain {
+
+    BasicInfoDomain basicInfoDomain;
+    CarInfoDomain carInfoDomain;
+    ContactEmergencyDomain contactEmergencyDomain;
+    ContactReferenceDomain contactReferenceDomain;
+    ResidentialStatusDomain residentialStatusDomain;
+
+}
--- EM-Rest-Server/src/main/java/com/example/emrestserver/domain/ResidentialStatusDomain.java
+++ EM-Rest-Server/src/main/java/com/example/emrestserver/domain/ResidentialStatusDomain.java
@@ -3,14 +3,12 @@ package com.example.emrestserver.domain;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.sql.Date;
-
 @Data
 @ToString
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ResidentialStatus {
+public class ResidentialStatusDomain {
     private String citizenOrResident;
     private String workAuthorization; // visa status
     private String startDate;
