c4c9189355ac8e854ba995208cfa34f04069ff03
GH-H <huangganghuan1@gmail.com>
2022-01-18 22:48:18 -0800

    update gitignore

---
+++ MainSysteam/src/app/login/login-form/login-form.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl} from '@angular/forms';
+import { VerifyUserService } from 'src/app/services/verify-user.service';
+import { errors } from 'src/app/entity/errors';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+
+  public nameControl = new FormControl();
+  public PasswordControl = new FormControl();
+  public loginAsHr: boolean = false;
+  errorObject: errors = {code:"", message:""}
+  
+  
+  constructor(private router: Router, private service: VerifyUserService ) { }
+
+  ngOnInit(): void {
+   
+  }
+
+  changerole(){
+    this.loginAsHr = !this.loginAsHr;
+   
+  }
+
+  verify(){
+   
+    this.service.verify(this.nameControl.value, this.PasswordControl.value).subscribe(
+      {
+        next: data => {
+            console.log(data);
+            this.router.navigate(['nav'])
+        },
+        error: e => {
+            
+           this.errorObject.code = e.status;
+           this.errorObject.message = e.error;
+            
+        }
+    })
+     
+
+    
+  }
+
+
+  
+  // verify(){
+    
+  //   this.service.verify(this.nameControl.value, this.PasswordControl.value).subscribe(
+  //     (data:any) =>{
+  //       this.cookie.set("1","12323232");
+  //       console.log('no error')
+  //       console.log(data);
+        
+        
+  //     },
+  //     (error) =>{
+  //       console.log('error')
+  //       console.log(error);
+  //     }
+
+  //   );
+     
+
+    
+  // }
+
+  // RedirectTohome(){
+  //   this.router.navigate(['nav']);
+  // }
+}
---
+++ MainSysteam/src/app/services/verify-user.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class VerifyUserService {
+
+
+  //private sso =  "auth/login";
+  private sso =  "api/hr/home";
+  
+  constructor(private http: HttpClient) { }
+
+
+  verify(email: string, psw: string):Observable<any>
+  {
+    
+    const body = {userName:email, 
+      password: psw};
+
+
+      console.log()
+      //let headers1 = new Headers();
+      //headers1.append('authentication', 'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwicm9sZSI6IkhSIiwiaWF0IjoxNjQyNTU4NzIyLCJleHAiOjE2NTQ1NTg3MjJ9.a14_CL3fDG-d-g7pfEU5cdfsarM6J884iujrvXPrgugtoken');
+      return this.http.get(this.sso);
+
+    // return this.http.post(this.sso,  body, {
+    //   responseType: 'text',
+    //   withCredentials: true,
+      
+    // }) 
+  }
+
+  
+ 
+}
