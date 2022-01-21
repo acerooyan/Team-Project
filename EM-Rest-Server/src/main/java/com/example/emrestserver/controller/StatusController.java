c61fa99da04a4aec3761ee176a08451690f7e33e
Ganghuan Huang <56037905+GH-H@users.noreply.github.com>
2022-01-21 09:12:34 -0800

    controller update

---
+++ EM-Rest-Server/src/main/java/com/example/emrestserver/controller/StatusController.java
@@ -0,0 +1,27 @@
+package com.example.emrestserver.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/status")
+    public ResponseEntity<Object> getStatus() {
+
+        try{
+            String status = null;
+            //todo: get status from applicationworkflow
+            
+            if(status != null && status.equals("complete")){
+                return ResponseEntity.ok().build();
+            }else{
+                return ResponseEntity.status(666).build();
+            }
+
+        }catch (Exception e){
+            System.out.println("error catch");
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+}
