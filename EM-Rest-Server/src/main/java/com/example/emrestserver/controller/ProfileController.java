bf1696d997551267ab20f615e9b75ab714b595e4
jcmei89 <jcmei89@gmail.com>
2022-01-21 09:05:33 -0800

    status

--- EM-Rest-Server/src/main/java/com/example/emrestserver/controller/ProfileController.java
+++ EM-Rest-Server/src/main/java/com/example/emrestserver/controller/ProfileController.java
@@ -1,25 +1,38 @@
 package com.example.emrestserver.controller;
 
+import com.example.emrestserver.constant.JwtConstant;
 import com.example.emrestserver.domains.profile.PersonalInfoDomain;
 import com.example.emrestserver.domains.profile.ProfileDomain;
+import com.example.emrestserver.security.util.CookieUtil;
+import com.example.emrestserver.security.util.JwtUtil;
+import com.example.emrestserver.service.EmployeeService1;
 import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true", allowedHeaders = "*")
 @RequestMapping("/jwt")
 public class ProfileController {
-
+    @Autowired
+    private EmployeeService1 employeeService1;
     @GetMapping("/profile")
-    public ResponseEntity<ProfileDomain> getProfile() {
+    public ResponseEntity<ProfileDomain> getProfile(ServletRequest servletRequest) {
 
+//        HttpServletRequest req = (HttpServletRequest) servletRequest;
+//        String token = CookieUtil.getValue(req, JwtConstant.JWT_COOKIE_NAME);
+//        String email = JwtUtil.getSubjectFromJwt(token);
+        String email = "jamesh970327@gmail.com";
         try{
             //get application list from database
         //List<HrProfilerDomain> hrProfilerDomainList = hrProfilerService.mapDocumentWithEmployee();
-        return ResponseEntity.ok().build();
+            ProfileDomain profileDomain = employeeService1.getDataReady(email);
+            return  ResponseEntity.ok().body(profileDomain);
         }catch (Exception e){
             System.out.println("error catch");
             return ResponseEntity.internalServerError().build();
@@ -102,8 +115,6 @@ public class ProfileController {
             return ResponseEntity.internalServerError().build();
         }
     }
-
-
 
 
 }
