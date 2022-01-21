5341ba97dff35015102bd5df7574302984225e4a
Ganghuan Huang <56037905+GH-H@users.noreply.github.com>
2022-01-20 22:47:11 -0800

    service update

---
+++ EM-Rest-Server/src/main/java/com/example/emrestserver/service/EmployeeService2.java
@@ -0,0 +1,15 @@
+package com.example.emrestserver.service;
+
+import com.example.emrestserver.domains.standalone.ContactEmergencyDomain;
+import com.example.emrestserver.entity.Contact;
+import com.example.emrestserver.entity.Person;
+import org.springframework.stereotype.Service;
+
+@Service("employeeService")
+public class EmployeeService2 {
+
+    public Contact getEmergencyByEmployeeId(Integer id){
+
+    }
+
+}
