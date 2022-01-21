d7347fad2c575c6a6e0028057b93021f3cd444f8
jcmei89 <jcmei89@gmail.com>
2022-01-20 22:35:23 -0800

    add 2 services

--- EM-Rest-Server/src/main/java/com/example/emrestserver/dao/EmployeeDao.java
+++ EM-Rest-Server/src/main/java/com/example/emrestserver/dao/EmployeeDao.java
@@ -1,11 +1,14 @@
 package com.example.emrestserver.dao;
 
+import com.example.emrestserver.entity.Address;
 import com.example.emrestserver.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class EmployeeDao {
@@ -29,6 +32,16 @@ public class EmployeeDao {
         Query query = session.createQuery("FROM Employee e WHERE e.person.email = :email");
         query.setParameter("email",email);
         return (Employee) query.getSingleResult();
+    }
+
+    public Address[] getAddressByPersonId(Integer personId){
+        Session session = getCurrentSession();
+        Query getAddress = session.createQuery("From Address WHERE Person.id =:id");
+        getAddress.setParameter("id", personId);
+        List<Address> addressList = (List<Address>)getAddress.getResultList();
+        Address[] addressArray = new Address[addressList.size()];
+        addressList.toArray(addressArray);
+        return addressArray;
     }
 
 }
--- EM-Rest-Server/src/main/java/com/example/emrestserver/service/EmployeeService1.java
+++ EM-Rest-Server/src/main/java/com/example/emrestserver/service/EmployeeService1.java
@@ -14,6 +14,6 @@ public class EmployeeService1 {
     }
 
     public Address[] getAddressByPersonId(Integer personId){
-        return null;
+        return employeeDao.getAddressByPersonId(personId);
     }
 }
