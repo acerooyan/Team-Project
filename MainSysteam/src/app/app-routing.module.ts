c76ea69445e8dc05b559b67c45bdd720b2c39306
Jimmy <hongjy4@uci.edu>
2022-01-22 21:36:52 -0800

    add filled info

--- MainSysteam/src/app/app-routing.module.ts
+++ MainSysteam/src/app/app-routing.module.ts
@@ -20,7 +20,12 @@ import { FileuploadComponent } from './test/fileupload/fileupload.component';
 import { EnavbarComponent } from './Ehome/enavbar/enavbar.component';
 import { PersonalInfoComponent } from './Ehome/personal-info/personal-info.component';
 import { EhomeComponent } from './Ehome/ehome/ehome.component';
-
+import { NavComponent } from './filled-info/nav/nav.component';
+import { step2Component } from './filled-info/step2/step2.component'
+import { step3Component } from './filled-info/step3/step3.component';
+import { step4Component } from './filled-info/step4/step4.component';
+import { step5Component } from './filled-info/step5/step5.component';
+import { step6Component } from './filled-info/step6/step6.component';
 
 const routes: Routes = [
 
@@ -39,14 +44,14 @@ const routes: Routes = [
       children: [
         
         {
-          path: 'hireNav', component: RegnavbarComponent,
+          path: 'hireNav', component: NavComponent,
           children: [
            
-            {path: 'step2', component: Step2Component},
-            {path: 'step3', component: Step3Component},
-            {path: 'step4', component: Step4Component},
-            {path: 'step5', component: Step5Component},
-            {path: 'step6', component: Step6Component},
+            {path: 'step2', component: step2Component},
+            {path: 'step3', component: step3Component},
+            {path: 'step4', component: step4Component},
+            {path: 'step5', component: step5Component},
+            {path: 'step6', component: step6Component},
             
           ]
         },
