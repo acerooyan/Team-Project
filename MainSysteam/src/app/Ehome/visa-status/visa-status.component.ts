8888e877d942a35c366aa4250d0c126cdfc4e58b
SukeyY <shu.y@wustl.edu>
2022-01-23 01:18:55 -0600

    change

--- MainSysteam/src/app/Ehome/visa-status/visa-status.component.ts
+++ MainSysteam/src/app/Ehome/visa-status/visa-status.component.ts
@@ -1,40 +1,79 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient ,  HttpRequest, HttpEvent} from '@angular/common/http';
 import { Observable, Observer } from 'rxjs';
+
+export class StatusInfo{
+  currentStep?: String;
+  nextStep?: String;
+  comment?: String;
+  documents?: string[];
+}
 @Component({
   selector: 'app-visa-status',
   templateUrl: './visa-status.component.html',
   styleUrls: ['./visa-status.component.css']
 })
-class StatusInfo{
-  currentStep?: String;
-  nextStep?: String;
-  comment?: String;
 
-}
 export class VisaStatusComponent implements OnInit {
   public statusInfo: StatusInfo = {};
-  // currentStep
-  // nextStep
-  // comment
 
-  // documents
+  selectedFiles?: FileList;
   private statusInfoUrl = "";
   constructor(private httpClient: HttpClient) { }
   ngOnInit(): void {
     this.getStatusInfo().subscribe(
       (data:any) =>{
         console.log(data);
-        this.statusInfo = {currentStep:data.}
+        this.statusInfo = {
+          currentStep:data.statusInfoDomain.currentStep,
+          nextStep:data.statusInfoDomain.nextStep,
+          comment: data.statusInfoDomain.comment,
+          documents:data.statusInfoDomain.documents
+        };
       }
     )
   }
-  selectedFiles?: FileList;
+
+
+
   selectFile(event: any): void {
     this.selectedFiles = event.target.files;
   }
+
   getStatusInfo():Observable<any>{
     return this.httpClient.get(this.statusInfoUrl,
     );
   }
+  reset() {
+    this.ngOnInit();
+  }
+
+
+
+  submitFile():Observable<HttpEvent<any>> {
+
+   const formData: FormData = new FormData();
+
+    if (this.selectedFiles) {
+
+      var file: File | null = this.selectedFiles.item(0);
+
+      if (file) {
+
+        console.log(file);
+        formData.append('file', file);
+
+
+      }
+    }
+    const req = new HttpRequest('POST', this.statusInfoUrl, formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+
+    return this.httpClient.request(req);
+
+  }
+
+
 }
--- MainSysteam/src/app/home/emp-visa-status-management/emp-visa-status-management.component.ts
+++ MainSysteam/src/app/home/emp-visa-status-management/emp-visa-status-management.component.ts
@@ -1,4 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient ,  HttpRequest, HttpEvent} from '@angular/common/http';
+import { Observable, Observer } from 'rxjs';
+import {Address} from "../../entity/address";
+import {Router} from "@angular/router";
+
+export class Status{
+  fullName?:String;
+  visa?:String;
+  startDate?:String;
+  endDate?:String;
+  dayLeft?:String;
+  documents?:string[];
+  currentStep?:String;
+  nextStep?:String;
+  comment?:String;
+  email?:String;
+  workflowStatus?:String;
+}
+
 
 @Component({
   selector: 'app-emp-visa-status-management',
@@ -6,10 +25,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./emp-visa-status-management.component.css']
 })
 export class EmpVisaStatusManagementComponent implements OnInit {
-
-  constructor() { }
-
+  statusList :Status[] = [] ;
+  test=[
+    {fullName: "sy",
+    visa:"OPT",
+    startDate:"1997",
+    endDate:"2021",
+    dayLeft:"10"
+  },
+    {fullName: "yy",
+      visa:"OPT",
+      startDate:"1999",
+      endDate:"2030",
+      dayLeft:"20"
+    },
+  ]
+  private statusInfoUrl = "";
+  constructor(private httpClient: HttpClient,private router: Router) { }
+  changedName?:String;
   ngOnInit(): void {
+    this.getStatusInfo().subscribe(
+      (data:any) =>{
+        console.log(data);
+        for(let i=0; i < data.statusDomains.length; i++) {
+          this.statusList.push(data.statusDomains[i]);
+        }
+      },
+      (error: any)=>{
+        if (error.status === 401) {
+          this.router.navigate(['']);
+        }
+        else{
+          console.log(error);
+        }
+
+      }
+    )
   }
+  getStatusInfo():Observable<any> {
+    return this.httpClient.get(this.statusInfoUrl);
+  }
+  public index=0
+  showDetail = [false,false];
+getDetail(i:number) {
+  this.index = i;
+this.showDetail[i] = !this.showDetail[i];
 
 }
+getArrayIndex(){
+  return this.showDetail[this.index];
+}
+
+}
+
