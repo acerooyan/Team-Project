bae631bcb6fb9385c8a30b6855af18a874cf8656
Ganghuan Huang <56037905+GH-H@users.noreply.github.com>
2022-01-23 14:22:29 -0800

    filter update

--- EM-Rest-Server/src/main/java/com/example/emrestserver/security/filter/JwtFilter.java
+++ EM-Rest-Server/src/main/java/com/example/emrestserver/security/filter/JwtFilter.java
@@ -16,36 +16,6 @@ import java.io.IOException;
 @WebFilter(urlPatterns = "/api/jwt/*")
 public class JwtFilter implements Filter {
 
-//    @Override
-//    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
-//
-//        res.setHeader("Access-Control-Allow-Origin", req.getHeader("Origin"));
-//        System.out.println(req.getHeader("Origin"));
-//        res.setHeader("Access-Control-Allow-Credentials", "true");
-//        res.setHeader("Access-Control-Allow-Methods", "*");
-//        res.setHeader("Access-Control-Max-Age", "3600");
-//        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
-//
-//        String token = CookieUtil.getValue(req, JwtConstant.JWT_COOKIE_NAME);
-//        System.out.println("+++++++++JwtFilter++++++++");
-//        System.out.println(token);
-//        if (token!=null) {
-//            String userName = JwtUtil.getSubjectFromJwt(token);
-//            System.out.println(userName);
-//            Claims claims = JwtUtil.getClaimsFromJwt(token);
-//            if (userName!=null) {
-//                req.setAttribute("userName", userName);
-//                filterChain.doFilter(req, res);
-//            } else {
-////                String authLoginUrl = getFilterConfig().getInitParameter("authLoginUrl");
-////                res.sendRedirect(authLoginUrl + "?redirect=" + req.getRequestURL());
-//            }
-//        } else {
-////            String authLoginUrl = getFilterConfig().getInitParameter("authLoginUrl");
-//            res.sendRedirect("authLoginUrl" + "?redirect=" + req.getRequestURL());
-//        }
-//    }
-
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
@@ -69,9 +39,6 @@ public class JwtFilter implements Filter {
         String token = CookieUtil.getValue(req, JwtConstant.JWT_COOKIE_NAME);
         System.out.println("+++++++++JwtFilter++++++++");
         System.out.println(token+"token");
-
-
-
 
         String userId = null;
         Claims claims = JwtUtil.getClaimsFromJwt(token);
